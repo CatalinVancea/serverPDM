4f17504e35d955b6777fabb61e571291bc89d928
Catalin Vancea <vancea_catalin24@yahoo.ro>
2020-12-02 02:34:33 +0200

    lab5

--- src/index.js
+++ src/index.js
@@ -1,28 +1,53 @@
-const Koa = require('koa');
+import Koa from 'koa';
+import WebSocket from 'ws';
+import http from 'http';
+import Router from 'koa-router';
+import bodyParser from "koa-bodyparser";
+import { timingLogger, exceptionHandler, jwtConfig, initWss, verifyClient } from './utils';
+import { router as noteRouter } from './note';
+import { router as authRouter } from './auth';
+import jwt from 'koa-jwt';
+import cors from '@koa/cors';
+
+
 const app = new Koa();
-const server = require('http').createServer(app.callback());
-const WebSocket = require('ws');
+const server = http.createServer(app.callback());
 const wss = new WebSocket.Server({ server });
+initWss(wss);
+
 const Router = require('koa-router');
 const cors = require('koa-cors');
 const bodyparser = require('koa-bodyparser');
 
-app.use(bodyparser());
 app.use(cors());
-app.use(async (ctx, next) => {
-  const start = new Date();
-  await next();
-  const ms = new Date() - start;
-  console.log(`${ctx.method} ${ctx.url} ${ctx.response.status} - ${ms}ms`);
-});
-app.use(async (ctx, next) => {
-  try {
-    await next();
-  } catch (err) {
-    ctx.response.body = { issue: [{ error: err.message || 'Unexpected error' }] };
-    ctx.response.status = 500;
-  }
-});
+app.use(timingLogger);
+app.use(exceptionHandler);
+app.use(bodyParser());
+
+const prefix = '/api';
+
+// public
+const publicApiRouter = new Router({ prefix });
+publicApiRouter
+    .use('/auth', authRouter.routes());
+app
+    .use(publicApiRouter.routes())
+    .use(publicApiRouter.allowedMethods());
+
+app.use(jwt(jwtConfig));
+
+
+// protected
+const protectedApiRouter = new Router({ prefix });
+protectedApiRouter
+    .use('/student', noteRouter.routes());
+app
+    .use(protectedApiRouter.routes())
+    .use(protectedApiRouter.allowedMethods());
+
+server.listen(3000);
+console.log('started on port 3000');
+
 
 
 class Student {
